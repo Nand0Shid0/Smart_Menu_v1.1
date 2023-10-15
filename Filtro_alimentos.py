d86e8d471cc9a67ce80d86e52d9a615d5e5a9df6
Nand0Shid0 <lolgurrola4545@gmail.com>
2023-10-14 22:35:32 -0600

    Raciones funcionando y terminado

--- Filtro_alimentos.py
+++ Filtro_alimentos.py
@@ -1,8 +1,12 @@
 import sqlite3
 
+db_name = "Alimentozz.db"
+var_conexion = sqlite3.connect(db_name)
+var_cursor = var_conexion.cursor()
+
 def get_proteina(grupo_alimentos, limite):
     print("Proteina\n")
-    sql = "SELECT Alimento, Proteina, Energia FROM Alimentos WHERE ID_Grupo=" + grupo_alimentos
+    sql = "SELECT Alimento, Proteina, Energia FROM Alimentos WHERE ID_Grupo=" + str(grupo_alimentos)
     var_cursor.execute(sql)
     rows = var_cursor.fetchall()
     Output(rows, limite)
@@ -18,18 +22,18 @@ def get_lipidos (grupo_alimentos, limite):
 
 def get_carbohidratos(grupo_alimentos, limite):
     print("Carbohidratos\n")
-    sql = "SELECT Alimento, Hidratos_de_carbono, Energia FROM Alimentos WHERE ID_Grupo=" + grupo_alimentos
+    sql = "SELECT Alimento, Hidratos_de_carbono, Energia FROM Alimentos WHERE ID_Grupo=" + str(grupo_alimentos)
     var_cursor.execute(sql)
     rows = var_cursor.fetchall()
     Output(rows, limite)
     print("\n")
 
-def get_energia(grupo_alimentos):
+def get_energia(grupo_alimentos, limite):
     print("Energia\n")
-    sql = "SELECT Alimento, Energia FROM Alimentos WHERE ID_Grupo=" + grupo_alimentos
+    sql = "SELECT Alimento, Energia FROM Alimentos WHERE ID_Grupo=" + str(grupo_alimentos)
     var_cursor.execute(sql)
     rows = var_cursor.fetchall()
-    Output(rows)
+    Output(rows, limite)
     print("\n")
 
 
@@ -39,9 +43,6 @@ def Output(rows, limite):
 
 def filtro(row, limite):
     #Elementos mayores a 5gr de cada tipo Proteina, Lipidos y Carbos por ejemplo seran tomados en cuenta
-    if row[1] > limite:
+    if float(row[1]) < limite:
         print(row)
 
-db_name = "Alimentozz.db"
-var_conexion = sqlite3.connect(db_name)
-var_cursor = var_conexion.cursor()
