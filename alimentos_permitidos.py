3004afcf43218e7af43a2170684f7d8c46dd1ac8
REAL_NAME = <83304047+Nand0Shid0@users.noreply.github.com>
2023-10-24 11:27:48 -0600

    Enfermedades agregadas y terminadas

--- alimentos_permitidos.py
+++ alimentos_permitidos.py
@@ -41,6 +41,34 @@ alimentos_hieprca = [
     "Pepitas, Avellanas, Piñones, Pistaches, Nuez de la India", "Azúcar, Miel, Mermeladas, Cajeta", "Agua pura"
 ]
 
+alimentos_dislipidemias = ["Arroz", "Avena", "Bolillo", "Tortilla y pan de caja", "Sandía", "Melón", "Guayaba", "Jícama", "Ciruela roja",
+                           "Durazno", "Todas las Verduras", "Frijol", "Lenteja", "Haba", "Garbanzo", "Soya", "Alubia", "Alverjón","Pollo", 
+                           "Pescado", "Res", "Clara de Huevo", "Queso Cottage y Requesón", "Jamón de Pechuga de Pavo","Pata de Res", 
+                           "Queso Panela y Cottage", "Leche Descremada y Yogur Light", "Aceitunas", "Chía", "Linaza","Aguacate y Aceites de Oliva", 
+                           "Canola", "Girasol","Soya", "Nuez", "Almendras", "Cacahuates", "Pepitas", "Avellanas", "Piñones", "Pistaches", "Nuez de la India", 
+                           "Agua pura"]
+
+alimentos_hipercalemia = ["Pan blanco", "Arroz blanco", "Arroz integral", "Espagueti", "Macarrones", "Tortilla de harina o maíz","Wafle de cuatro pulgadas", 
+                          "Manzana", "Pera", "Sandía", "Mandarina", "Melocotón", "Limón", "Naranja", "Ciruela","Membrillo", "Calabacín crudo y cocido", "Cebolla", 
+                          "Col", "Coliflor", "Repollo", "Lombarda", "Nabos cocidos",    "Pepinos", "Pimientos crudos", "Berenjenas", "Espárragos cocidos", "Lechuga", 
+                          "Huevo", "Queso Ricota", "Requesón",    "Helado de Vainilla", "Maní", "Almendras", "Mermelada"]
+
+alimentos_hipocalemia = [ "Camote", "Papa", "Pan de Salvado", "Papas fritas", "Platano", "Papaya", "Ciruela pasa", "Pasas", "Mango", "Kiwi",
+                            "Naranja", "Melon", "Pera", "Tomate", "Champiñones", "Bruselas", "Calabaza", "Brocoli", "Frijoles", "Lentejas",    
+                            "Salmon", "Pavo", "Res", "Yogur", "Leche entera", "Leche de soya", "Melazas", "Chocolate", "Maní", "Semillas de Girasol"
+                            ]
+
+alimentos_osteoporosis = ["Camote", "Higos", "Fresas", "Naranjas", "Higos secos", "Zanahoria", "Col", "Nabo", "Brócoli", "Calabaza",    
+                          "Alubias", "Queso Parmesano", "Queso Manchego", "Queso Gruyere"]
+
+alimetos_tiroiditis_hashimoto = [
+        "Manzana", "Limón", "Alcachofa", "Hinojo", "Canela", "Azafrán","Albaricoque", "Lima", "Arugula", "Habichuelas", "Clavos de olor", "Sal marina sin yodo","Aguacate", "Mango", "Spárragos", "Col rizada", "Ajo", "Chalotes","Guineo", "Zarzas", "Bok choi", "Puerro", "Jengibre", "Cúrcuma","Moras", "Nectarina", "Brócoli", "Lechuga","Arándano", "Naranja", "Coles de Bruselas", "Hongos", "Órganos","Melón", "Papaya", "Repollo", "Ruibarbo","Cerezas", "Melocotón", "Coliflor", "Guisante", "Caldo de hueso", "Riñón","Clementinas", "Pera", "Apio", "Espinaca", "Hígado", "Corazón","Coco", "Caqui", "Acelga", "Zapallo","Dátiles", "Ciruela", "Berza", "Berro",
+        "Higos", "Piña", "Pepino", "Proteína animal","Uvas", "Granada","Pomelo", "Frambuesa", "Carne", "Pollo",
+        "Guava", "Fresa", "Bisonte", "Pavo","Mirtilo", "Mandarina", "Búfalo", "Pato","Melón verde", "Sandía", "Cordero", "Puerco",
+        "Kiwi", "Plátano", "Pescado", "Conejo","Crustáceos", "Venado","Raíces", "Hierbas aromáticas","Remolacha", "Nabo", "Albahaca", "Menta", "Vinagre de dátiles","Zanahoria", "Rábano", "Hojas de laurel", "Perejil", "Manzana fruta seca","Apio", "Nabo", "Nabo sueco", "Manzanilla", "Menta piperita", "Anchoas", "Olivas","Jicama", "Chalote", "Cebollino", "Romero", "Harina de arrurruz", "Salmón en conserva", "Cebolla", "Batata", "Cilantro", "Salvia", "Harina de coco", "Sardinas","Chirivía", "Ñame", "Eneldo", "Hierba buena", "Hojuelas de coco", "Tuna en conserva","Lavanda", "Estragón", "Vinagre de coco", "Vinagre de ciruela","Hierba de limón", "Tomillo", "Aminos de coco", "Leche de coco sin","Mejorana", "Harina de plátano", "Azúcar","Harina de yuca", "Crema de coco sin", "Azúcar","Harina de otoe", "Azúcar","Harina de batata", "Gelatina sin sabor", "Chucrut", "Kombucha", "Miel cruda", "Azúcar morena","Aguacate", "Oliva, extra virgen", "Vegetales", "Kefir de agua", "Azúcar de dátil", "Azúcar de coco","Jarabe de arce", "Polvo de algarroba","Mantequilla clarificada", "Azúcar de arce", "Stevia","Coco", "Melaza", "Jugo de caña evaporado"
+]
+
+
 enfermedades_alimentos_permitidos = {
     "Sobrepeso": alimentos_metabolicos,
     "Obesidad": alimentos_metabolicos,
@@ -71,6 +99,11 @@ enfermedades_alimentos_permitidos = {
     "VIH": alimentos_hieprca,
     "Cancer": alimentos_hieprca,
     "Quemados": alimentos_hieprca,
+    "Tiroides de Hashimoto": alimetos_tiroiditis_hashimoto,
+    "Osteoporosis" : alimentos_osteoporosis,
+    "Hipercalemia" : alimentos_hipercalemia,
+    "Hipocalemia": alimentos_hipocalemia,
+    "Dislipidemias": alimentos_dislipidemias,
     "Otra": []
 }
 
